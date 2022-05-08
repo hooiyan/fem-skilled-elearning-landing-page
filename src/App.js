a1c9d1fe08c921973299943d7fc998b6ac3c45c1
hooiyan <hoecoding@gmail.com>
2022-05-08 18:58:09 +0800

    Install packages

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { render } from 'react-dom';
+
+export function App() {
+  return (
+    <div>
+      <h1>Frontend Mentor Skilled E-Learning</h1>
+    </div>
+  );
+}
+
+render(<App />, document.getElementById('app'));
